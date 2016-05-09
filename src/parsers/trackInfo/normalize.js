a7ac4e2494a2bf79db22ff494785bf971c649179
Borys Ihnatyev <paur.link@gmail.com>
2016-05-09 20:01:33 +0300

    refactor(mapReplace)

--- src/parsers/trackInfo/normalize.js
+++ src/parsers/trackInfo/normalize.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const transliterate = require('transliteration');
+const mapReplace = require('../../util/string/mapReplace');
 
 const doubleSpacesAndDownSlashesRX = /[\s\s,_]+/g;
 const webAddressesRX = /(?:https?|ftp):\/\/[\n\S]+/g;
@@ -8,9 +9,10 @@ const space = ' ';
 const empty = '';
 
 module.exports = function normalize(value) {
-    return String(transliterate(value))
-        .toLowerCase()
-        .replace(doubleSpacesAndDownSlashesRX, space)
-        .replace(webAddressesRX, empty)
-        .trim();
+    return mapReplace(
+        String(transliterate(value))
+    )(
+        [doubleSpacesAndDownSlashesRX, space],
+        [webAddressesRX, empty]
+    ).trim().toLowerCase();
 };
